218c68eaeb990a2d58cb6e3c140193821b7cf28e
jamesalbers <92996849+jamesalbers@users.noreply.github.com>
2021-10-27 09:14:47 -0500

    Add files via upload

---
+++ PowerShell FInal Exam.ps1
@@ -0,0 +1,65 @@
+#PowewerShell - Final Exam
+#Student Name:
+#Course #:
+#Date:
+##########################################################
+
+#Question #1
+#No need to add scripts for this question
+
+
+#region Question #2
+#submitted by
+#date
+
+
+#endregion 
+
+
+#region Question #3
+#submitted by
+#date
+
+#endregion
+
+#region Question #4
+#submitted by
+#date
+
+#endregion
+
+#region Question #5
+#submitted by
+#date
+
+#endregion
+
+#region Question #6 
+#submitted by
+#date
+
+#endregion
+
+#region Question #7 
+#submitted by
+#date
+
+#endregion
+
+#region Question #8
+#submitted by
+#date
+
+#endregion
+
+#region Question #9
+#submitted by
+#date
+
+#endregion
+
+#region Question #10
+#submitted by
+#date
+
+#endregion 
---
+++ README.md
@@ -0,0 +1,3 @@
+# FinalExam
+Students will add their code to the "PowerShell Final Exam.ps1' file
+The 'Capstone Scripts' folder contains the sample scripts referenced on page 3 of the final exam
